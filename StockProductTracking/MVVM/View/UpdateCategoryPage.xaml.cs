9d4cf18280218b48e31006e3e47806d071be0dc8
emirongrr <emirongrr@gmail.com>
2022-11-24 04:42:24 +0300

    validation rules for categoryviewmodelbase

--- StockProductTracking/MVVM/View/UpdateCategoryPage.xaml.cs
+++ StockProductTracking/MVVM/View/UpdateCategoryPage.xaml.cs
@@ -24,5 +24,7 @@ namespace StockProductTracking.MVVM.View
         {
             InitializeComponent();
         }
+
+      
     }
 }
--- StockProductTracking/MVVM/ViewModel/CategoryViewModelBase.cs
+++ StockProductTracking/MVVM/ViewModel/CategoryViewModelBase.cs
@@ -1,16 +1,46 @@
 ﻿using StockProductTracking.Core;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace StockProductTracking.MVVM.ViewModel
 {
-    internal abstract class CategoryViewModelBase : ObservableObject
+    internal abstract class CategoryViewModelBase : ObservableObject , IDataErrorInfo
     {
         public int CategoryId { get; set; }
         public string CategoryTitle { get; set; }
-        
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = string.Empty;
+                if (columnName == "CategoryTitle")
+                {
+                    if (string.IsNullOrEmpty(this.CategoryTitle))
+                        result = "Kategori ismi boş olamaz";
+
+                    else if (this.CategoryTitle.Length < 3)
+                        result = "Minimum 3 karakter boyutunda olmalıdır.";
+
+                    else if (!Regex.IsMatch(this.CategoryTitle, @"^[a-zA-Z]+$"))
+                        result = "Sadece harf kabul edilir. [A-z,a-z]";
+
+                }
+
+                return result;
+            }
+        }
     }
+
 }
+
