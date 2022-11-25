41f3aadfe435387b796daad343438eb7ee4bce22
delledev <lucadelle2@gmail.com>
2022-11-25 17:46:07 +0300

    c

--- StockProductTracking/Utils/DashboardHandler.cs
+++ StockProductTracking/Utils/DashboardHandler.cs
@@ -1,4 +1,5 @@
-﻿using LiveCharts;
+﻿using Google.Protobuf.WellKnownTypes;
+using LiveCharts;
 using LiveCharts.Wpf;
 using StockProductTracking.MVVM.Model;
 using System;
@@ -12,42 +13,19 @@ namespace StockProductTracking.Utils
 {
     internal class DashboardHandler
     {
-        private List<String> GetUniqueItems(ObservableCollection<Order> orders)
-        {
-            List<String> returnValue = new List<String>();
-            foreach (Order order in orders)
-            {
-                if (!returnValue.Contains(order.OrderProductTitle))
-                {
-                    returnValue.Add(order.OrderProductTitle);
-                }
-            }
-            return returnValue;
-        }
         public SeriesCollection GetChartData()
         {
             SeriesCollection chartSeries = new SeriesCollection();
-
-            ObservableCollection<Order> orders = new Connect().GetAcceptedOrders();
-            List<String> UniqueProductNameList = GetUniqueItems(orders);
-            
-            foreach (String uniqueProductName in UniqueProductNameList)
+            ChartValues<Double> values = new ChartValues<Double>();
+            LineSeries ls = new LineSeries
             {
-                ChartValues<Double> values = new ChartValues<Double>();
-                LineSeries ls = new LineSeries
-                {
-                    Title = uniqueProductName,
-                    Values = values
-                };
-                chartSeries.Add(ls);
-                foreach (Order o in orders)
-                {
-                    if (!(o.OrderProductTitle == uniqueProductName))
-                    {
-                        continue;
-                    }
-                    values.Add(o.OrderProductPrice * o.OrderProductCount);
-                }
+                Title = "Toplam Kar",
+                Values = values
+            };
+            chartSeries.Add(ls);
+            foreach (Order order in new Connect().GetAcceptedOrders())
+            {
+                values.Add(order.OrderProductCount * order.OrderProductPrice);
             }
             return chartSeries;
         } 
