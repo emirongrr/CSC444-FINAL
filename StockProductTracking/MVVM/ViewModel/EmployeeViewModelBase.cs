1672ad9cab15e7b6e4b88c0f4e564ac27ee367f7
emirongrr <emirongrr@gmail.com>
2022-12-05 09:15:22 +0300

    validation rules for employee but have wrong password rule logic

--- StockProductTracking/MVVM/ViewModel/EmployeeViewModelBase.cs
+++ StockProductTracking/MVVM/ViewModel/EmployeeViewModelBase.cs
@@ -11,9 +11,9 @@ namespace StockProductTracking.MVVM.ViewModel
         public string EmployeeLastName { get; set; }
         public string EmployeeUsername { get; set; }
         public string EmployeePassword { get; set; }
+        public string EmployeePasswordAgain { get; set; }
         public string EmployeeEmail { get; set; }
         public bool EmployeeIsAdmin { get; set; }
-
 
 
         public string Error
@@ -36,6 +36,51 @@ namespace StockProductTracking.MVVM.ViewModel
 
                     else if (!Regex.IsMatch(this.EmployeeFirstName, @"^[a-zA-Z]+$"))
                         result = "Sadece harf kabul edilir. [A-z,a-z]";
+
+                }
+                if (columnName == "EmployeeLastName")
+                {
+                    if (string.IsNullOrEmpty(this.EmployeeLastName))
+                        result = "Soyad boş olamaz";
+
+                    else if (this.EmployeeLastName.Length < 3)
+                        result = "Minimum 3 karakter boyutunda olmalıdır.";
+
+                    else if (!Regex.IsMatch(this.EmployeeLastName, @"^[a-zA-Z]+$"))
+                        result = "Sadece harf kabul edilir. [A-z,a-z]";
+
+                }
+                if (columnName == "EmployeeUsername")
+                {
+                    if (string.IsNullOrEmpty(this.EmployeeUsername))
+                        result = "Kullanıcı Adı boş olamaz";
+
+                    else if (this.EmployeeUsername.Length < 3)
+                        result = "Minimum 3 karakter boyutunda olmalıdır.";
+
+                }
+                if (columnName == "EmployeeEmail")
+                {
+                    if (string.IsNullOrEmpty(this.EmployeeEmail))
+                        result = "Email boş olamaz";
+                    else if (!Regex.IsMatch(this.EmployeeEmail, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+                        result = "Uygun mail giriniz. (example@mail.com)";
+                   
+                }
+                if (columnName == "EmployeePassword")
+                {
+                    if (string.IsNullOrEmpty(this.EmployeePassword))
+                        result = "Şifre boş olamaz";
+                    else if (!(EmployeePassword == EmployeePasswordAgain))
+                        result = "BOŞBOŞBOŞBOŞBOŞB";
+
+                }
+                if (columnName == "EmployeePasswordAgain")
+                {
+                    if (string.IsNullOrEmpty(this.EmployeePasswordAgain))
+                        result = "Şifre boş olamaz";
+                    else if (!(EmployeePassword == EmployeePasswordAgain))
+                        result = "BOŞBOŞBOŞBOŞBOŞB";
 
                 }
 
